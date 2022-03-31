d8cdaf522cd6c717f6de25480f08a4f2ac58bbe1
facundoherrera1104 <3blocksmedia.latam@gmail.com>
2022-04-01 06:09:16 +0900

    Updated readme

--- src/hooks/useCloneFactoryContract.js
+++
@@ -1,8 +0,0 @@
-import useContract from "./useContract";
-
-import cloneFactoryContractAbi from "abis/CloneFactory.json";
-
-const useCloneFactoryContract = () =>
-  useContract(process.env.REACT_APP_NFT_IMPLEMENTATION_CONTRACT_ADDRESS, cloneFactoryContractAbi, true);
-
-export default useCloneFactoryContract;
--- src/hooks/useImplementationContract.js
+++
@@ -1,8 +0,0 @@
-import useContract from "./useContract";
-
-import implementationContractAbi from "abis/Implementation.json";
-
-const useImplementationContract = () =>
-  useContract(process.env.REACT_APP_NFT_IMPLEMENTATION_CONTRACT_ADDRESS, implementationContractAbi, true);
-
-export default useImplementationContract;
